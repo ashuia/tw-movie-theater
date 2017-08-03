a4ac311407777362f0787f6191924213cf7f08e0
hui <1018997102@qq.com>
2017-08-03 17:50:49 +0800

    M

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 'use strict';
 var alltag=[];
-var typetag=['动作','喜剧','恐怖','剧情','动画','悬疑','惊悚','家庭','犯罪','奇幻','冒险'];
-var placetag=['内地','Hongkong and Taiwan','America','England','Korean','Japanese','Australian']
+const typetag=['动作','喜剧','恐怖','剧情','动画','悬疑','惊悚','家庭','犯罪','奇幻','冒险','音乐','西部'];
+const placetag=['全部地区','内地','港台','美国','英国','德国','韩国','日本','澳大利亚'];
+const timetag=['2017','2016','2015','2014','2013','2012','2011','2010','2009','2008','2007'];
 function abruf(a) {
-    if(a=='alltype')
-        alltag[0]=null;
-    if(a=='allplace')
-        alltag[1]=null;
+    alltag=[]
+    if(a=='全部类型')
+    alltag[0]='';
+    if(a=='全部地区')
+    alltag[1]='';
+    if(a=='全部时间')
+        alltag[2]='';
     for(let item of typetag)
         if(a==item)
             alltag[0]=item;
-  for(let item of placetag)
-      if(a==item)
-          alltag[1]=item;
-
+    for(let item of placetag)
+        if(a==item)
+            alltag[1]=item;
+    for(let item of timetag)
+        if(a==item)
+            alltag[2]=item;
 showmovie()}
-function showmovie() {
-    var div=`<div class="col-sm-3"><img src="../content/04.jpg"><p align="center">name</p></div> `
+
+function showmovie(moviearr) {
+    $('#picture').remove('#col-sm-3');
+    for(let item of moviearr)
+    var div=`<div class="col-sm-3"><img src='${item.url}'><p align="center">${item.name}</p></div> `
    $('#picture').append(div)
 
 }
-function printfselect(alltage) {
-    let genreStr = alltage.join(",");
-    $.ajax({
-        type:'GET',
-        url:'http://localhost:9998/movies?genreName='+genreStr,
-        data:alltage,
-        crossDomain:true,
-        success:function () {
-            abruf(a);
-        }
-    })
+function sortmovie(movi) {
 
 }
