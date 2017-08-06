003591d5d6972088b7b300f12f5308da1f190ccc
hui <1018997102@qq.com>
2017-08-06 21:51:12 +0800

    m

--- js/main.js
+++ js/main.js
@@ -19,16 +19,90 @@ function abruf(a) {
             alltag[1]=item;
     for(let item of timetag)
         if(a==item)
-            alltag[2]=item;
-showmovie()}
+            alltag[2]=item;}
+//showmovie()}
 
-function showmovie(moviearr) {
-    $('#picture').remove('#col-sm-3');
-    for(let item of moviearr)
-    var div=`<div class="col-sm-3"><img src='${item.url}'><p align="center">${item.name}</p></div> `
-   $('#picture').append(div)
+
+//显示分类情况
+var data=new Object()
+data.name='hui';
+data.image=''
+function showselect(data){
+    for(let item of data){
+        let div=`<div class='col-sm-3' >
+                  <a title=点击查看详情 style="background-color: #9acfea">
+                   <img class="double-border" src='${data.image}'>
+                   <p>${data.name}</p></div>`;
+       $('#picture').append(div);
+    }
+}
+function printfselect(alltag) {
+    $.ajax({
+        type:'get',
+        url:'http://localhost:8080/search?alltag'+alltag,
+        crossDomain:true,
+        success:function (data) {
+            showselect(data);
+        },
+        error:function (jqXHR,textstatus,errThrown) {
+            alert('error'+textstatus+''+errThrown);
+        }
+    })
 
 }
-function sortmovie(movi) {
+/*动画效果
+$(document).ready(function (){
+    $('div.col-sm-3').hover(function () {
+        $(this).find('.details').fadeTo('fast', 0.7);
+    },function () {
+        $(this).find('.details').fadeOut('fast');
 
-}
+    })
+
+});*/
+/*缩放效果
+$(document).ready(function () {
+    $('#picture')
+        .bind('mouseenter mouseleave',function (evevnt) {
+          var size=evevnt.type =='mouseeter'?85:75;
+          var padding=evevnt.type =='mouseter'?0:5;
+          $(this).find('img').animate({
+              width:size,
+              height:size,
+              paddingTop:padding,
+              paddingLeft:padding,
+          })
+        })
+
+}); */
+/*放大图片
+$(document).ready(function () {
+    $('#picture')
+    .bind('mouseenter mouseleave',function (event) {
+        var $image = $(this).find('img');
+        if(!$image.is(':animated')
+        ||event.type=='mouseleave'){
+    }
+    debugger;
+    var size=event.type =='mouseleave'?150:170;
+    var padding =event.type =='mouseenter'?0:20;
+    $image.animate({
+        width:size,
+        height:size,
+        paddingTop:padding,
+        paddingLeft:padding
+    })
+
+    })
+    });*/
+
+$(function() {
+    $(window).scroll(function() {
+        var top = $(window).scrollTop()+100;
+        var left= $(window).scrollLeft();
+        $("#move").css({ left:left + "px", top: top + "px" });
+    });
+});
+/*$("#goTopBtn").click(function(){
+    var sc=$(window).scrollTop();
+    $('body,html').animate({scrollTop:0},500)*/
